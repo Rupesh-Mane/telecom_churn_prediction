ad7bbecc8fff1b929add2ec89197f7547e105a5f
Rupesh-Mane <manerupesh317@gmail.com>
2026-07-20 22:57:43 +0530

    Initial project structure for telecom churn prediction

---
+++ src/config/config.py
@@ -0,0 +1,17 @@
+# Dataset Path
+
+RAW_DATA_PATH = "data/raw/customer_churn_telecom.csv"
+
+
+
+
+# ======================
+# Model Settings
+# ======================
+
+
+TARGET_COLUMN = "Churn"
+
+TEST_SIZE = 0.2
+
+RANDOM_STATE = 42
